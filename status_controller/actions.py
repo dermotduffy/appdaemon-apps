54c711b14aeefa2bad19c32cb2a3a1f372f0a4b4
Dermot Duffy <dermot.duffy@gmail.com>
2019-11-03 21:09:57 -0800

    Fuller locking for actions.

--- status_controller/actions.py
+++ status_controller/actions.py
@@ -30,16 +30,14 @@ SONOS_SERVICE_MEDIA_STOP = 'media_player/media_stop'
 
 class ActionBase(object):
   def __init__(self, app, complete_callback, **kwargs):
+    # kill_action() may be called from a different thread.
+    self._lock = threading.RLock()
+
     self._app = app
     self._complete_callback = complete_callback
     self._kwargs = kwargs
     self._is_finished = False
-
     self._priority = self._pop_argument(scc.CONF_PRIORITY)
-
-    # kill_action() may be called from a different thread, so lock
-    # protect the underlying _complete_action and _is_finished as appropriate.
-    self._lock = threading.RLock()
 
   def get_priority(self):
     return self._priority
@@ -65,7 +63,8 @@ class ActionBase(object):
     self._complete_callback(self)
 
   def _pop_argument(self, argument, default=None):
-    return self._kwargs.pop(argument, default)
+    with self._lock:
+      return self._kwargs.pop(argument, default)
 
   def is_finished(self):
     with self._lock:
@@ -90,9 +89,12 @@ class TimedActionBase(ActionBase):
     super()._complete_action(force=force)
 
   def _schedule_action_complete(self):
-    self._complete_timer_handle = self._app.run_in(
-        self._complete_action,
-        self._length)
+    with self._lock:
+      if self._is_finished:
+        return
+      self._complete_timer_handle = self._app.run_in(
+          self._complete_action,
+          self._length)
 
   def _cancel_timer(self, timer_handle):
     self._app.cancel_timer(timer_handle)
@@ -104,7 +106,6 @@ class SonosAction(TimedActionBase):
 
     self._entity_id = entity_id
     self._primary = primary
-
     self._volume = self._pop_argument(scc.CONF_SONOS_VOLUME)
 
     scc.log(self._app, self, 'Sonos entity: %s (Primary is: %s)' % (
@@ -123,10 +124,11 @@ class SonosAction(TimedActionBase):
 
   def prepare(self):
     super().prepare()
+
     self._unjoin()
 
+    # Primary does not need to join itself.
     if not self._is_primary():
-      # Primary does not need to join itself.
       self._join()
 
     self._set_volume()
@@ -142,6 +144,9 @@ class SonosAction(TimedActionBase):
     app.call_service(SONOS_SERVICE_RESTORE)
 
   def _stop_media(self):
+    with self._lock:
+      if self._is_finished:
+        return
     scc.log(self._app, self, 'Stopping play on: %s' % self._entity_id)
     self._app.call_service(
         SONOS_SERVICE_MEDIA_STOP,
@@ -149,6 +154,9 @@ class SonosAction(TimedActionBase):
 
   def _set_volume(self):
     if self._volume:
+      with self._lock:
+        if self._is_finished:
+          return
       scc.log(self._app, self, 'Setting volume to %f for: %s' % (
           self._volume, self._entity_id))
       self._app.call_service(
@@ -157,6 +165,9 @@ class SonosAction(TimedActionBase):
           volume_level=self._volume)
 
   def _unjoin(self):
+    with self._lock:
+      if self._is_finished:
+        return
     # Need to unjoin even if there's only 1 entity (as it may already be joined
     # to something else, we do not know).
     scc.log(self._app, self, 'Unjoining: %s' % self._entity_id)
@@ -165,6 +176,9 @@ class SonosAction(TimedActionBase):
         entity_id=self._entity_id)
 
   def _join(self):
+    with self._lock:
+      if self._is_finished:
+        return
     scc.log(self._app, self, 'Joining \'%s\' to: %s' %
         (self._entity_id, self._primary))
     self._app.call_service(
@@ -199,13 +213,14 @@ class SonosTTSAction(SonosAction):
 
     if self._chime:
       self._action_chime()
-      self._speak_timer_handle = self._app.run_in(
-        self._action_speak,
-        self._chime_length)
     else:
       self._action_speak()
 
   def _action_chime(self):
+    with self._lock:
+      if self._is_finished:
+        return
+
     scc.log(self._app, self, 'Chiming on %s: \'%s\'' % (
         self._entity_id, self._chime))
     self._app.call_service(
@@ -214,7 +229,16 @@ class SonosTTSAction(SonosAction):
         media_content_id=self._chime,
         media_content_type='music')
 
+    with self._lock:
+      if not self._is_finished:
+        self._speak_timer_handle = self._app.run_in(
+            self._action_speak,
+            self._chime_length)
+
   def _action_speak(self, kwargs=None):
+    with self._lock:
+      if self._is_finished:
+        return
     scc.log(self._app, self, 'Speaking on %s: \'%s\'' % (
         self._entity_id, self._message))
     self._app.call_service(
@@ -233,6 +257,10 @@ class SonosPlayMediaAction(SonosAction):
     super().action()
     if not self._is_primary():
       return
+
+    with self._lock:
+      if self._is_finished:
+        return
 
     scc.log(self._app, self, 'Playing media on %s: \'%s\'' % (
         self._entity_id, self._media))
@@ -269,6 +297,10 @@ class LightActionBase(TimedActionBase):
       self._turn_on()
 
   def _turn_on_with_args(self, entity_id=None, **kwargs):
+    with self._lock:
+      if self._is_finished:
+        return
+
     entity_id = entity_id or self._entity_id
     scc.log(self._app, self, 'Turning on: %s (%s)' % (entity_id, kwargs))
     self._app.turn_on(
@@ -279,6 +311,10 @@ class LightActionBase(TimedActionBase):
     return self._turn_on_with_args(**self._kwargs)
 
   def _turn_off_with_args(self, entity_id=None, **kwargs):
+    with self._lock:
+      if self._is_finished:
+        return
+
     entity_id = entity_id or self._entity_id
     scc.log(self._app, self, 'Turning off: %s (%s)' % (entity_id, kwargs))
     self._app.turn_off(
@@ -331,7 +367,6 @@ class LightActionBase(TimedActionBase):
 class SimpleLightAction(LightActionBase):
   def action(self):
     super().action()
-
     if self._action == scc.CONF_ACTION_LIGHT_TURN_ON:
       self._turn_on()
     elif self._action == scc.CONF_ACTION_LIGHT_TURN_OFF:
@@ -364,6 +399,10 @@ class BreathingLightAction(LightActionBase):
 
   def action(self):
     super().action()
+    with self._lock:
+      if self._is_finished:
+        return
+
     self._breathe_timer_handle = self._app.run_every(
         self._breathe,
         self._app.datetime(),
@@ -377,11 +416,15 @@ class BreathingLightAction(LightActionBase):
     super()._complete_action(force=force)
 
   def _cancel_breathe_timer(self):
-    if self._breathe_timer_handle is not None:
-      self._cancel_timer(self._breathe_timer_handle)
-      self._breathe_timer_handle = None
+    with self._lock:
+      if self._breathe_timer_handle is not None:
+        self._cancel_timer(self._breathe_timer_handle)
+        self._breathe_timer_handle = None
 
   def _breathe(self, kwargs):
+    with self._lock:
+      if self._is_finished:
+        return
     if self._beats_remaining <= 0:
       self._cancel_breathe_timer()
       self._complete_action()
@@ -409,6 +452,9 @@ class NotifyAction(ServiceAction):
 
   def action(self):
     super().action()
+    with self._lock:
+      if self._is_finished:
+        return
     kwargs = { 'message': self._message }
     if self._title is not None:
       kwargs['title'] = self._title
